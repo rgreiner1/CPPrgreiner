ef44a8f27e8bbc60dd056336db8e9b8dc4a0fed6
rgreiner1 <138033554+rgreiner1@users.noreply.github.com>
2024-04-25 15:52:33 +0200

    cpp06

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rgreiner <rgreiner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:26:15 by rgreiner          #+#    #+#             */
-/*   Updated: 2024/04/24 11:33:34 by rgreiner         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:00:00 by rgreiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,63 @@ ScalarConverter::~ScalarConverter(){
     
 }
 
-void    ScalarConverter::convertDouble(double toConvert){
+ScalarConverter::ScalarConverter(const ScalarConverter& src){
+    *this = src;
+}
+
+ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &src){
+    this->toChar = src.toChar;
+    this->toInt = src.toInt;
+    this->toDouble = src.toDouble;
+    this->toFloat = src.toFloat;
+    return *this;
+}
+
+
+void    ScalarConverter::convertDouble(double toConvert , std::string stringNbr){
     toChar = static_cast<char>(toConvert);
     toInt = static_cast<int>(toConvert);
     toFloat = static_cast<float>(toConvert);
     toDouble = toConvert;
-    if (toChar)
-        std::cout << "Char : " << toChar << std::endl;
+    if (toChar  && toConvert >= 0.0 && toConvert <= 127.0 && isprint(toConvert) != 0)
+        std::cout << "Char : " << "'" << toChar << "'" << std::endl;
     else
         std::cout << "Char : Non displayable" << std::endl;
-    std::cout << "Int : " << toInt << std::endl;
-    std::cout << "Float : " << toFloat << std::endl;
-    std::cout << "Double : " << toDouble << std::endl;
+    if (checkOverflow(stringNbr, "int") == 0)
+        std::cout << "Int : " << toInt << std::endl;
+    else
+        std::cout << "Int : Overflow" << std::endl;
+    if (toFloat == toInt)
+        std::cout << "Float : " << toFloat << ".0f" << std::endl;
+    else
+         std::cout << "Float : " << toFloat << "f" << std::endl;
+    if (toDouble == toInt)
+        std::cout << "Double : " << toDouble << ".0" << std::endl;
+    else
+        std::cout << "Double : " << toDouble << std::endl;    
 }
 
-void    ScalarConverter::convertFloat(float toConvert){
+void    ScalarConverter::convertFloat(float toConvert, std::string stringNbr){
     toChar = static_cast<char>(toConvert);
     toInt = static_cast<int>(toConvert);
     toFloat = toConvert;
     toDouble = static_cast<double>(toConvert);
-    if (toChar)
-        std::cout << "Char : " << toChar << std::endl;
+    if (toChar  && toConvert >= 0.0f && toConvert <= 127.0f && isprint(toConvert) != 0)
+        std::cout << "Char : " << "'" << toChar << "'" << std::endl;
     else
         std::cout << "Char : Non displayable" << std::endl;
-    std::cout << "Int : " << toInt << std::endl;
-    std::cout << "Float : " << toFloat << std::endl;
-    std::cout << "Double : " << toDouble << std::endl;
+    if (checkOverflow(stringNbr, "int") == 0)
+        std::cout << "Int : " << toInt << std::endl;
+    else
+        std::cout << "Int : Overflow" << std::endl;
+    if (toFloat == toInt)
+        std::cout << "Float : " << toFloat << ".0f" << std::endl;
+    else
+         std::cout << "Float : " << toFloat << "f" << std::endl;
+    if (toDouble == toInt)
+        std::cout << "Double : " << toDouble << ".0" << std::endl;
+    else
+        std::cout << "Double : " << toDouble << std::endl;   
 }
 
 void    ScalarConverter::convertChar(char toConvert){
@@ -53,10 +84,10 @@ void    ScalarConverter::convertChar(char toConvert){
     toInt = static_cast<int>(toConvert);
     toFloat = static_cast<float>(toConvert);
     toDouble = static_cast<double>(toConvert);
-    std::cout << "Char : " << toChar << std::endl;
+    std::cout << "Char : " << "'" << toChar << "'" << std::endl;
     std::cout << "Int : " << toInt << std::endl;
-    std::cout << "Float : " << toFloat << std::endl;
-    std::cout << "Double : " << toDouble << std::endl;
+    std::cout << "Float : " << toFloat << ".0f" << std::endl;
+    std::cout << "Double : " << toDouble << ".0" << std::endl;
 }
 
 void    ScalarConverter::convertInt(int toConvert){
@@ -64,13 +95,13 @@ void    ScalarConverter::convertInt(int toConvert){
     toInt = toConvert;
     toFloat = static_cast<float>(toConvert);
     toDouble = static_cast<double>(toConvert);
-    if (toChar && toConvert >= 0 && toConvert <= 127)
+    if (toChar && toConvert >= 0 && toConvert <= 127 && isprint(toConvert) != 0)
         std::cout << "Char : " << "'" << toChar << "'" << std::endl;
     else
         std::cout << "Char : Non displayable" << std::endl;
     std::cout << "Int : " << toInt << std::endl;
-    std::cout << "Float : " << toFloat << std::endl;
-    std::cout << "Double : " << toDouble << std::endl;
+    std::cout << "Float : " << toFloat << ".0f" << std::endl;
+    std::cout << "Double : " << toDouble << ".0" << std::endl;
 }
 
 void    ScalarConverter::handleFloat(std::string toConvert){
@@ -87,29 +118,96 @@ void    ScalarConverter::handleDouble(std::string toConvert){
     std::cout << "Double : " << toConvert << std::endl;
 }
 
+int     ScalarConverter::checkOverflow(std::string toConvert, std::string type){
+    std::stringstream ss;
+    ss << toConvert;
+    int n;
+    double d;
+    if (type == "int")
+    {
+        ss >> n;
+        if(!ss.fail())
+            return (0);
+        else
+            return (1);
+    }
+    if (type == "double")
+    {
+        ss >> d;
+        if(!ss.fail())
+            return (0);
+        else
+            return (1);
+    }
+    return (0);
+}
+
 void    ScalarConverter::convert(std::string toConvert){
+    size_t i;
+    int j;
+    int l;
+
+    i = 0;
+    j = 0;
+    l = 0;
     if (toConvert == "-inff" || toConvert == "+inff" || toConvert == "nanf")
         this->handleFloat(toConvert);
     else if (toConvert == "-inf" || toConvert == "+inf" || toConvert == "nan")
         this->handleDouble(toConvert);
-    else if (toConvert.rfind("f") == toConvert.length() - 1)
-    {
-        float floatconvert = atof(toConvert.c_str());
-        this->convertFloat(floatconvert); 
-    }
-    else if (toConvert.find(".") != toConvert.npos)
-    {
-        double doubleconvert = atof(toConvert.c_str());
-        this->convertDouble(doubleconvert);
-    }
     else if (toConvert.length() == 1 && isdigit(toConvert[0]) == 0 && isprint(toConvert[0]) != 0)
     {
         char charconvert = toConvert[0];
         this->convertChar(charconvert);
     }
+    else if (toConvert.rfind("f") == toConvert.length() - 1)
+    {
+        j = 0;
+        l = 0;
+        while(isdigit(toConvert[i]) || toConvert[i] == 'f' || toConvert[i] == '.')
+        {
+            if(toConvert[i] == 'f')
+                j++;
+            if(toConvert[i] == '.')
+                l++;
+            i++;
+        }
+        if (j <= 1 && l <= 1)
+        {
+            float floatconvert = atof(toConvert.c_str());
+            this->convertFloat(floatconvert, toConvert); 
+        }
+        else
+            std::cout << "Is not a valid input" << std::endl;
+        i = 0;
+    }
+    else if (toConvert.find(".") != toConvert.npos || checkOverflow(toConvert, "int") == 1)
+    {
+        j = 0;
+        while(isdigit(toConvert[i]) || toConvert[i] == '.')
+        {
+            if(toConvert[i] == '.')
+                j++;
+            i++;
+        }
+        if (j <= 1)
+        {
+            double doubleconvert = atof(toConvert.c_str());
+            this->convertDouble(doubleconvert, toConvert);
+        }
+        else
+            std::cout << "Is not a valid input" << std::endl;
+        i = 0;
+    }
     else
     {
-        int intconvert = atoi(toConvert.c_str());
-        this->convertInt(intconvert);
+        while (isdigit(toConvert[i]) != 0 || toConvert[i] == '-' || toConvert[i] == '+')
+            i++;
+        if (toConvert.size() == i)
+        {
+            int intconvert = atoi(toConvert.c_str());
+            this->convertInt(intconvert);
+        }
+        else
+            std::cout << "Is not a valid input" << std::endl;
     }
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rgreiner <rgreiner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:26:18 by rgreiner          #+#    #+#             */
-/*   Updated: 2024/04/24 11:10:39 by rgreiner         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:18:04 by rgreiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,14 @@ private:
 public:
     ScalarConverter(/* args */);
     ~ ScalarConverter();
+    ScalarConverter(const ScalarConverter& src);
+    ScalarConverter	&operator=(const ScalarConverter &src);
+    int     checkOverflow(std::string toConvert, std::string type);
     void    convert(std::string toConvert);
     void    convertChar(char toConvert);
     void    convertInt(int toConvert);
-    void    convertFloat(float toConvert);
-    void    convertDouble(double toConvert);
+    void    convertFloat(float toConvert, std::string stringNbr);
+    void    convertDouble(double toConvert, std::string stringNbr);
     void    handleFloat(std::string toConvert);
     void    handleDouble(std::string toConvert);
 };
